b05c963d8f811e7a04e37d63bb10b435d6c47281
Felipe Almeida <felipe.almeida@eniac.com.br>
2019-08-09 20:47:02 -0300

    Serviço Front End Cadastro de Funcionários

--- src/App.js
+++ src/App.js
@@ -10,17 +10,16 @@ class App extends Component {
       name:'',
       email:''
     },
-    newEmployeeModal : false
+    editEmployeeData : {
+      name:'',
+      email:''
+    },
+    newEmployeeModal : false,
+    editEmployeeModal : false
   }
 
   componentWillMount() {
-
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
-      this.setState({
-        //employees : [{'title':'123456','nome':'Felipe','email':'felipe@2far.com.br'}]
-        employees : [response.data]
-      })
-    });
+    this._refreshList();
   }
 
   toggleNewEmployee() {
@@ -29,9 +28,55 @@ class App extends Component {
     });
   }
 
+  toggleEditEmployee() {
+    this.setState({
+      editEmployeeModal: ! this.state.editEmployeeModal
+    });
+  }
+
   addEmployee() {
-    axios.post('https://jsonplaceholder.typicode.com/todos/1',this.state.newEmployeeData).then((response) => {
-      console.log(response.data);
+
+    axios.post('https://jsonplaceholder.typicode.com/todos',this.state.newEmployeeData).then((response) => {
+      let { employees } = this.state;
+
+      employees.push(response.data);
+    
+      this.setState({ employees,newEmployeeModal:false,newEmployeeData : {
+        name:'',
+        email:''
+      } });
+    });
+  }
+
+  updateEmployee() {
+    let { name, email} = this.state.editEmployeeData;
+    axios.put('https://jsonplaceholder.typicode.com/todos' + this.state.editEmployeeData.id , {
+      name,email
+    } ).then((response) =>  {
+      this._refreshList();
+    });
+      
+  }
+
+  _refreshList(){
+    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
+      this.setState({
+        //employees : [{'title':'123456','nome':'Felipe','email':'felipe@2far.com.br'}]
+        employees : [response.data]
+      })
+    });
+  }
+
+  editEmployee(id,name,email) {
+    this.setState({
+      editEmployeeData: { id,name,email}, editEmployeeModal : ! this.editEmployeeModal
+    });
+
+  }
+
+  deleteEmployee() {
+    axios.delete('https://jsonplaceholder.typicode.com/todos' + this.state.editEmployeeData.id).then((response) =>{
+      this._refreshList();
     });
   }
 
@@ -45,8 +90,8 @@ class App extends Component {
           <td>{e.title}</td>
           <td>{e.title}</td>
           <td>
-            <Button color="success" size="sm" className="mr-2">Edit</Button>
-            <Button color="danger" size="sm">Delete</Button>
+            <Button color="success" size="sm" className="mr-2" onClick={this.editEmployee.bind(this,e.name,e.email)}>Edit</Button>
+            <Button color="danger" size="sm" onClick={this.deleteEmployee.bind(this,e.id)} >Delete</Button>
           </td>
         </tr>
       )
@@ -71,6 +116,7 @@ class App extends Component {
           </Table>
 
           <Button color="primary" onClick={this.toggleNewEmployee.bind(this)}>Add Employee</Button>
+          
           <Modal isOpen={this.state.newEmployeeModal} toggle={this.toggleNewEmployee.bind(this)} className={this.props.className}>
             <ModalHeader toggle={this.toggleNewEmployee.bind(this)}>Add New Employee</ModalHeader>
             <ModalBody>
@@ -95,6 +141,33 @@ class App extends Component {
             <ModalFooter>
               <Button color="primary" onClick={this.addEmployee.bind(this)}>Save</Button>{' '}
               <Button color="secondary" onClick={this.toggleNewEmployee.bind(this)}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+
+          <Modal isOpen={this.state.editEmployeeModal} toggle={this.toggleEditEmployee.bind(this)} className={this.props.className}>
+            <ModalHeader toggle={this.toggleEditEmployee.bind(this)}>Edit Employee</ModalHeader>
+            <ModalBody>
+              
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input id="name" type="text" value={this.state.editEmployeeData.name} onChange={(e) => { 
+                let { editEmployeeData } = this.state;
+                editEmployeeData.name = e.target.value;
+                this.setState({ editEmployeeData });
+              } } />
+
+              <Label for="email">Email</Label>
+              <Input id="name" type="text" value={this.state.editEmployeeData.email} onChange={(e) => { 
+                let { editEmployeeData } = this.state;
+                editEmployeeData.email = e.target.value;
+                this.setState({ editEmployeeData });
+              } } />
+            </FormGroup>
+
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.updateEmployee.bind(this)}>Save</Button>{' '}
+              <Button color="secondary" onClick={this.toggleEditEmployee.bind(this)}>Cancel</Button>
             </ModalFooter>
           </Modal>
 
